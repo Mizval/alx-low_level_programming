bef972fc998055b0b0fc1e25141532a67ee4cff7
Mizval <valentinerebecca70@gmail.com>
2023-02-24 09:17:19 +0100

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putcher('1');
+	{
+		if (j >= 10)
+		_putcher('1');
 	_putchar (j % 10 + '0');
-		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 	}
 }
