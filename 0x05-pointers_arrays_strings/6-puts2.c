96cf5c184c8a59825f5f6d3afe2183fa78b5be72
mizval <mizval@DESKTOP-D9AG6ID>
2023-02-27 15:09:41 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,23 +9,15 @@
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	while (str[1] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[0]);
-	}
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
